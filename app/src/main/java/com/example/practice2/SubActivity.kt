c4c030d90566ea3f752df9be4465fbf4be0533d6
blanco <skrk309@naver.com>
2021-07-19 04:45:22 +0900

    revising... To do list : swipe, maintain checkbox, recyclerview data move
    complete swipe to delete, drag and drop
    
    not complet maintain checkbox, recyclerview data move

--- app/src/main/java/com/example/practice2/SubActivity.kt
+++ app/src/main/java/com/example/practice2/SubActivity.kt
@@ -8,9 +8,11 @@ import com.example.practice2.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
     lateinit var binding: ActivitySubBinding
-    companion object{
+
+    companion object {
         const val Music = "app_preferences"
     }
+
     private lateinit var mPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,20 +20,22 @@ class SubActivity : AppCompatActivity() {
         binding = ActivitySubBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        var touchFlag = false
         mPreferences = getSharedPreferences(SubActivity.Music, MODE_PRIVATE);
         val preferencesEditor: SharedPreferences.Editor = mPreferences.edit()
 
-        var intent = Intent(this,MainActivity::class.java)
+        var intent = Intent(this, MainActivity::class.java)
 
-        if(mPreferences.getInt("music_img",0) == 0) {
+        if (mPreferences.getInt("music_img", 0) == 0) {
             finish()
-        }
-        else{
+        } else {
             binding.img3Sub.setImageResource(mPreferences.getInt("music_img", 0))
             binding.img2Sub.setImageResource(mPreferences.getInt("music_img", 0))
             binding.txt2Sub.setText(mPreferences.getString("music_name", null))
             binding.txt3Sub.setText(mPreferences.getString("music_contents", null))
+            binding.progressBar2.setProgress(mPreferences.getInt("music_length", 0))
         }
+
         binding.img3Sub.setOnClickListener()
         {
             finish()
@@ -40,5 +44,21 @@ class SubActivity : AppCompatActivity() {
         {
             finish()
         }
+
+        binding.img4Sub.setOnClickListener()
+        {
+            if(touchFlag == false)
+            {
+                binding.img4Sub.setImageResource(R.drawable.heart2)
+                touchFlag = true
+            }
+            else
+            {
+                binding.img4Sub.setImageResource(R.drawable.heart)
+                touchFlag = false
+            }
+        }
     }
 }
+
+
